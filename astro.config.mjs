aa46959fdb4af8de26f7a741abf9729dc52d9824
TeluguTechTalk <skrishna2599@gmail.com>
2026-05-16 21:17:55 +0530

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,26 +10,6 @@ export default defineConfig({
   site: SITE,
   adapter: cloudflare(),
   integrations: [
-    sitemap({
-      customPages: [
-        `${SITE}/`,
-        `${SITE}/about`,
-        `${SITE}/java`,
-        `${SITE}/java/checked-vs-unchecked-exceptions`,
-        `${SITE}/java/common-exception-mistakes`,
-        `${SITE}/java/custom-exceptions`,
-        `${SITE}/java/exception-best-practices`,
-        `${SITE}/java/exception-handling-overview`,
-        `${SITE}/java/exception-handling`,
-        `${SITE}/java/exception-propagation`,
-        `${SITE}/java/java-exception-handling-interview-questions`,
-        `${SITE}/java/real-world-exception-handling-examples`,
-        `${SITE}/java/throw-vs-throws`,
-        `${SITE}/java/throwable-hierarchy`,
-        `${SITE}/java/try-catch-finally`,
-        `${SITE}/java/try-with-resources`,
-        `${SITE}/java/what-is-exception-handling`,
-      ],
-    }),
+    sitemap() 
   ],
 });
